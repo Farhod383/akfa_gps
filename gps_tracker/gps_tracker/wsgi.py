dfc1771278b6fab6b7f0e2a5030c2c9aaff3b200
Gulinur <ruziyevagulinur@gmail.com>
2025-10-19 20:26:18 +0500

    fix: corrected settings import path

--- gps_tracker/gps_tracker/wsgi.py
+++ gps_tracker/gps_tracker/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gps_tracker.gps_tracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gps_tracker.settings')
+
 
 
 
